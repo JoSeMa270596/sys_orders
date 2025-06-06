82ce6bf22fe799a7cd322ff1afca5f8926e6fcd1
JoSeMa270596 <112894489+JoSeMa270596@users.noreply.github.com>
2025-06-06 11:39:40 -0400

    feat(backend): Add dependenci injection

---
+++ Backend/Backend.Infrastructure/IoC/BackendDI.cs
@@ -0,0 +1,39 @@
+using Backend.Application.Providers;
+using Backend.Application.Services;
+using Backend.Domain.Repositories;
+using Backend.Infrastructure.EntityFramework.Context;
+using Backend.Infrastructure.EntityFramework.Repositories;
+using Backend.Infrastructure.Provider;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backend.Infrastructure.IoC;
+
+public static class BackendDI
+{
+    public static IServiceCollection RegisterDataBase(this IServiceCollection collection, IConfiguration configuration)
+    {
+        string connectionString = configuration["ConnectionStrings:localConnection"];
+        collection.AddDbContext<BackenDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            }
+        );
+        return collection;
+    }
+
+    public static IServiceCollection RegisterServices(this IServiceCollection collection)
+    {
+        //add your Services
+        //Activity
+        collection.AddTransient<UserService>();
+        return collection;
+    }
+
+    public static IServiceCollection RegisterRepositories(this IServiceCollection collection)
+    {
+        collection.AddTransient<IUserRepository, UserRepository>();
+        return collection;
+    }
+}
