58ab8e649abe9934ec6c64add33435250fb0a03e
JoSeMa270596 <112894489+JoSeMa270596@users.noreply.github.com>
2025-06-06 12:39:07 -0400

    feat(dashboard):  Add navigation

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './features/user/user.component';
+import { ProductComponent } from './features/product/product.component';
+import { OrderComponent } from './features/order/order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'order', component: OrderComponent },
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: '**', redirectTo: '/user' }
+];
